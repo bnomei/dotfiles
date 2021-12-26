4490952d6ddae9ce970fd99bdd701d3b8e4717fb
Bruno Meilick <bnomei@users.noreply.github.com>
2021-12-26 17:50:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,29 @@
 
 apt-get update
 apt-get install -y \
-    jq
+  curl \
+  git \
+  jq \
+  sudo \
+  less \
+  wget \
+  unzip \
+  bat \
+  exa \
+  fd \
+  ripgrep \ 
+  neovim
+  
+# Install NerdFont Jetbrains Mono
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+sudo unzip JetBrainsMono.zip -d /usr/share/fonts
+sudo fc-cache -f -v
+
+# https://starship.rs/
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# Copy dotfiles
+cp -f ~/dotfiles/.bashrc ~/.bashrc
+
+# Cleanup
+sudo apt-get autoremove -y
